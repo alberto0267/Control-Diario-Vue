63cf43dac0dd10ff807907af1dfcf6546a230494
BlancoAlberto <blancoalberto.r@gmail.com>
2025-04-02 21:22:34 +0200

    Control Diario Vue

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import logo from "./assets/logo.png";
+import "./styles/login.css";
+import Login from "./components/Login.vue";
+</script>
+
+<template>
+  <Login />
+</template>
---
+++ src/components/Login.vue
@@ -0,0 +1,22 @@
+<script setup>
+import logo from "../assets/logo3d.png";
+import "../styles/login.css";
+</script>
+
+<template>
+  <div class="login-container">
+    <img :src="logo" alt="Logo" class="login-logo" />
+
+    <div class="login-box">
+      <label for="email">Email</label>
+      <input type="email" id="email" placeholder="Value" />
+
+      <label for="password">Password</label>
+      <input type="password" id="password" placeholder="Value" />
+
+      <a href="#" class="forgot-link">Olvide Contraseña</a>
+    </div>
+
+    <button class="login-button">Iniciar sesión</button>
+  </div>
+</template>
