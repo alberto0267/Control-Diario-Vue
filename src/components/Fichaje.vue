fcfa11e0c20de8ae0803586382535716e1fae6f0
BlancoAlberto <blancoalberto.r@gmail.com>
2025-04-10 17:19:45 +0200

    Alb

--- src/components/Fichaje.vue
+++ src/components/Fichaje.vue
@@ -3,41 +3,108 @@ import Header from "../components/Header.vue";
 import Footer from "../components/Footer.vue";
 import { ref, onMounted, onUnmounted } from "vue";
 import "../styles/fichaje.css";
+
+const getHora = () => String(new Date().getHours()).padStart(2, "0");
+
+// Devuelve los minutos actuales con 2 dígitos (por ejemplo, "05")
+const getMinutos = () => String(new Date().getMinutes()).padStart(2, "0");
+
+const getSegundos = () => String(new Date().getSeconds()).padStart(2, "0");
+
+const getHoraCompleta = () => `${getHora()}:${getMinutos()}:${getSegundos()}`;
+
 const tiempo = ref("");
 
+const horas = ref("");
+const minutos = ref("");
+const segundos = ref("");
+
+const actualizarHoras = () => {
+  const now = new Date();
+  horas.value = getHora();
+};
+
+const actualizarMinutos = () => {
+  const now = new Date();
+  minutos.value = getMinutos();
+};
+
+const actualizarSegundos = () => {
+  const now = new Date();
+  segundos.value = getSegundos();
+
+  segundos.value = String(now.getSeconds()).padStart(2, "0");
+};
 const actualizarTiempo = () => {
   const now = new Date();
-  tiempo.value = now.toLocaleTimeString("es-ES", {
-    hour12: false,
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-  });
+  tiempo.value = getHoraCompleta();
 };
 
 let intervalos;
+let intervalosH;
+let intervalosM;
+let intervalosS;
 
 /* Esto se usa en vez de settiemOut porque  el time out lo llama una vez a menos que sea recursiva */
 onMounted(() => {
   actualizarTiempo();
+  actualizarHoras();
+  actualizarMinutos();
+  actualizarSegundos();
+
+  intervalosS = setInterval(actualizarSegundos, 1000);
+  intervalosH = setInterval(actualizarHoras, 1000);
+  intervalosM = setInterval(actualizarMinutos, 1000);
   intervalos = setInterval(actualizarTiempo, 1000);
 });
 
 onUnmounted(() => {
   clearInterval(intervalos);
+  clearInterval(intervalosH);
+  clearInterval(intervalosM);
+  clearInterval(intervalosS);
 });
 </script>
-
 <template>
+  <div class="dashboard">
+    <Header />
+    <main class="main-container">
+      <div class="clock">
+        <span class="hours">{{ horas }}:</span>
+        <span class="minutos">{{ minutos }}:</span>
+        <span class="segundos">{{ segundos }}</span>
+      </div>
+
+      <div class="action-buttons buttonsTop">
+        <button class="icon-button">
+          <img src="../assets/fichajeEntrada.png" alt="Entrada" />
+        </button>
+        <button class="icon-button selected">
+          <img src="../assets/fichajeSalida.png" alt="Salida" />
+        </button>
+      </div>
+
+      <div class="action-buttonsBottom">
+        <button class="icon-button">
+          <img src="../assets/descanso.png" alt="Descanso" />
+        </button>
+      </div>
+    </main>
+    <Footer />
+  </div>
+</template>
+<!-- <template>
   <div class="dashboard">
     <Header />
     <main>
       <div class="clock">
-        {{ tiempo }}
+        <span class="hours">{{ horas }}:</span>
+        <span class="hours">{{ minutos }}: </span>
+        <span class="hours">{{ segundos }} </span>
       </div>
     </main>
   </div>
   <Footer />
-</template>
+</template> -->
 
 <style scoped></style>
