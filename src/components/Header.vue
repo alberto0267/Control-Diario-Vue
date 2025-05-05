07f1e415f012aff8721324b803c5c62f72d801dd
gato4x4 <victoramirezserra@gmail.com>
2025-05-05 11:12:21 +0200

    profile menu inserted

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,14 +7,36 @@ const router = useRouter();
 const goToLobby = () => {
   router.back();
 };
+
+const toggleDropdown = () => {
+  const dropdown = document.querySelector(".dropdown-menu");
+  if (dropdown) {
+    dropdown.classList.toggle("show");
+  }
+};
+
+const logout = () => {
+  console.log("Cerrando sesión...");
+  router.push("/login");
+};
 </script>
 
 <template>
   <header class="dashboard-header">
-    <img src="../assets/logo3d.png" alt="Logo" class="logo" @click="goToLobby" />
-    <div class="user-info">
+    <img
+      src="../assets/logo3d.png"
+      alt="Logo"
+      class="logo"
+      @click="goToLobby"
+    />
+    <div class="user-info" @click="toggleDropdown">
       <span>Hola, Fulano</span>
       <img src="../assets/googleIcon.png" alt="User" class="user-icon" />
+      <div class="dropdown-menu">
+        <p>Nombre: Fulano</p>
+        <p>Tienda: FPeuroformac</p>
+        <button @click.stop="logout">Cerrar sesión</button>
+      </div>
     </div>
   </header>
 </template>
